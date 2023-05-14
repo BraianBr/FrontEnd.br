c0f91ece79a0c63fff8c3142afe45ed55c19ab18
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-14 18:21:27 -0300

    Errores corregidos
    jwt.expiration y userDateilimpl @service, Pom modificacion.

--- ProyectoIntegrador_RodriguezBraianEzequiel/BackEnd/src/main/java/com/projecto/integrador/br/Security/Service/UserDetailsImpl.java
+++ ProyectoIntegrador_RodriguezBraianEzequiel/BackEnd/src/main/java/com/projecto/integrador/br/Security/Service/UserDetailsImpl.java
@@ -11,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsImpl implements UserDetailsService{
     @Autowired
     UsuarioService usuarioService;
