5d2c184b3042faf3b5ce02b6b1c53c00079d2638
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-15 20:40:11 -0300

    Revert "modificacion url"
    This reverts commit 588363e2ae08d37545e4a3c2ebd62a1b97d2e4ca.

--- ProyectoIntegrador_RodriguezBraianEzequiel/FrontEnd/src/app/service/educacion.service.ts
+++ ProyectoIntegrador_RodriguezBraianEzequiel/FrontEnd/src/app/service/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  URL = 'https://backend-br.fly.dev/educacion/';
+  URL = 'http://localhost:8080/educacion/';
 
   constructor(private httpClient : HttpClient) { }
 
