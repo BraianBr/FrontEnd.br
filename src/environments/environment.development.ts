4a3359682e4dc6bc082d23611564596479823619
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-18 20:42:58 -0300

    Enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,2 +1,2 @@
 export const environment = {
-    URL:' http://localhost:8080/' };
+    URL:'https://backend-br.fly.dev/' };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    URL: 'backend-br.fly.dev/'
+    URL: 'https://backend-br.fly.dev/'
   };
