0e8f0679e01d2f73b5dde5f35da2f3117c532fbb
Braian Ezequiel Rodriguez <Braian2.0roket@gmail.com>
2023-05-18 23:46:47 -0300

    Chevere

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,8 +16,8 @@ const routes: Routes = [
   {path: 'editexp/:id', component: EditExperienciaComponent},
   {path: 'nuevaedu', component: NeweducacionComponent},
   {path: 'editedu/:id', component: EditeducacionComponent},
-  { path: 'newskill', component: NewSkillComponent},
-  { path: 'editskill/:id', component: EditSkillComponent}
+  {path: 'newskill', component: NewSkillComponent},
+  {path: 'editskill/:id', component: EditSkillComponent}
 ];
 
 @NgModule({
